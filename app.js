2a1293b43b0665ae1bd7fb06e74f6cdb1c7df507
TiagoGomesBastos <tgbinter@gmail.com>
2026-04-28 15:39:33 -0300

    produtos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/usuarios', require('./routes/usuariorote'));
-
+app.use('/produtos', require('./routes/produtoRoutes'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
