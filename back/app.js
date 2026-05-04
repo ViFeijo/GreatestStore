eadcccdbe9694eca3b5d9190afee51978ee39fd8
ArthurChemello <arthur.chemello@gmail.com>
2026-05-04 16:55:03 -0300

    Quase, nada

--- back/app.js
+++ back/app.js
@@ -5,10 +5,7 @@ const app = express();
 app.use(express.json());
 app.use('/usuarios', require('./routes/usuariorote'));
 app.use('/produtos', require('./routes/produtoRoutes'));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
 
-//import (ES Modules) uso 
-// Dentro de supabaseClient.js ou onde você configurou o Supabase
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
--- back/routes/produtoRoutes.js
+++ back/routes/produtoRoutes.js
@@ -5,7 +5,6 @@ const { checkRole } = require('../middlewares/rules');
 
 router.get('/', produtoController.listartodos);
 router.get('/:id', produtoController.buscarPorId);
-
 router.post('/', verifyToken, checkRole('vendedor'), produtoController.criar);
 router.put('/:id', verifyToken, checkRole('vendedor'), produtoController.atualizar);
 router.delete('/:id', verifyToken, checkRole('vendedor'), produtoController.deletar);
