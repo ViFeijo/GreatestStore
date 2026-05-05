1eec593adfd8e01592760af2fd3155ceb8cc5292
Rodrigo Schmitt de Almeida <rodrigoschmitt0508@gmail.com>
2026-05-05 18:01:27 -0300

    feat: add Footer component and integrate it into the layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Header from "@/components/layout/Header/Header";
+import Footer from "@/components/layout/Footer";
 
 const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+      <Footer />
     </html>
   );
 }
