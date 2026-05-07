53306fd051243fd0167537fe68924a55ab3b49d5
Rodrigo Schmitt de Almeida <rodrigoschmitt0508@gmail.com>
2026-05-07 00:41:25 -0300

    Add CORS middleware to enable cross-origin requests

--- back/app.js
+++ back/app.js
@@ -2,6 +2,16 @@ require('dotenv').config({ path: './Secret.env' });
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
