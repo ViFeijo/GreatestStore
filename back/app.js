eeb2c04e5f001128c40bf970a99db74c82f1e347
vifeijo <vibfeijo@gmail.com>
2026-05-08 01:14:12 -0300

    nao sei oq aconteceu com o app.js

--- back/app.js
+++ back/app.js
@@ -2,7 +2,15 @@ require('dotenv').config({ path: './Secret.env' });
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 const authRoutes = require('./routes/authRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
