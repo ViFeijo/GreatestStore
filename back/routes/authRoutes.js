1e5cdea0408259f6b6db3a9456e084aea6210356
rodrigo <rodrigoschmitt0508@gmail.com>
2026-05-09 23:48:44 -0300

    removendo o back, vou criar um novo repositorio para isso e mandar o link no git do front

--- back/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/registrar/cliente', authController.registrarCliente);
-router.post('/registrar/vendedor', authController.registrarVendedor);
-router.post('/login', authController.login);
-
-module.exports = router;
